4ce9fa55af229288d84e19feb0017efcf0ccc2d8
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2024-12-05 09:14:48 +0530

    linked with libc

--- build.zig
+++ build.zig
@@ -3,12 +3,22 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const exe = b.addExecutable(.{
-        .name = "correptio",
-        .root_source_file = b.path("src/main.zig"),
+    const debuggee = b.addExecutable(.{
+        .name = "debuggee",
+        .root_source_file = b.path("src/debuggee.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(debuggee);
+
+    const debugger = b.addExecutable(.{
+        .name = "debugger",
+        .root_source_file = b.path("src/debugger.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    debugger.linkLibC();
+
+    b.installArtifact(debugger);
 }
