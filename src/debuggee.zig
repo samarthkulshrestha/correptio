d523ac90fd4cbe4dd1a26dfb5a807d4ffde43154
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2024-12-06 13:13:09 +0530

    removed debug print statements

--- src/debuggee.zig
+++ src/debuggee.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+fn eat(_: anytype) void {}
+
 const GlobalVarTracker = enum(u54) {
     a,
     b,
@@ -12,11 +14,18 @@ fn do_print_loop(val: usize) void {
     const x: i32 = 4 + @as(i32, @intCast(val));
     const y: u8 = 5 + @as(u8, @intCast(val));
     my_global_var_pog += 1;
-    std.debug.print("loop iter {d} {d} {d} {d} {any}\n", .{ x, y, val, my_global_var_pog, my_global_var_2 });
+    eat(x);
+    eat(y);
+    // disabling for demonstration purposes,
+    // the dubugger tries to step into zig's
+    // `debug` and we get a permission error
+    // as it tries to print `stderr`
+    // std.debug.print("loop iter {d} {d} {d} {d} {any}\n", .{ x, y, val, my_global_var_pog, my_global_var_2 });
 }
 
 fn do_print_loop_2(val: usize) void {
-    std.debug.print("another loop iter {d}\n", .{val});
+    // std.debug.print("another loop iter {d}\n", .{val});
+    eat(val);
 }
 
 pub fn main() !void {
